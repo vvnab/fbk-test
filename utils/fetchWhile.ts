6a634fffda3f86592fd3309c5ac8761f3bd09c24
Виталий Набережный <naberezhniy@taxi21.ru>
2020-04-05 07:19:20 +0300

    fix fetchWhile

--- utils/fetchWhile.ts
+++ utils/fetchWhile.ts
@@ -1,28 +1,29 @@
 import fetch from "node-fetch";
 
-export default async function (url, maxCount = 10, wait = 5000) {
-  var count = 0;
-  function loop() {
-    return fetch(url)
-      .then(async function (result: any) {
-        if (++count > maxCount) {
-          throw new Error("Max count loop exceeded");
-        } else {
-          if (result.status === 200) {
-            return await result.json();
-          } else if (result.status === 403) {
-            wait *= count;
-            console.log(`403: waiting ${wait / 1000} sec`);
-            return new Promise(function (resolve) {
-              setTimeout(function () {
-                resolve(loop());
-              }, wait);
-            });
-          } else {
-            throw new Error(result.statusText);
-          }
-        }
-      });
+async function _wait(ms: number) {
+  return new Promise((resolve: Function) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export default async function fetchWhile(url, maxCount = 10, wait = 5000) {
+  let count = 0;
+  const loop = async () => {
+    if (++count > maxCount) {
+      throw new Error("Max count loop exceeded");
+    } else {
+      const result = await fetch(url);
+      if (result.status === 200) {
+        return result.json();
+      } else if (result.status === 403) {
+        console.log(`403: waiting ${wait / 1000} sec`);
+        await _wait(wait);
+        wait += wait;
+        return loop();
+      } else {
+        throw new Error(result.statusText);
+      }
+    }
   }
   return loop();
 }
